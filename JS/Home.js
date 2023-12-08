cf9cec50b4f6a64ed7b74a5ba393a059fdc74771
Dre <aciobirca97@gmail.com>
2023-12-08 17:17:40 +0000

    Updated information

--- JS/Home.js
+++ JS/Home.js
@@ -3,4 +3,6 @@ particlesJS.load('hero', 'JS/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-
+  particlesJS.load('about', 'JS/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
