2e246147cb34758e055fd29f74329f43dfae0a72
Dre <aciobirca97@gmail.com>
2023-12-08 00:31:36 +0000

    Fixed bubbles on hero

--- JS/Home.js
+++ JS/Home.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'JS/particles.json', function() {
+particlesJS.load('hero', 'JS/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
