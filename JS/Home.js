63682e26f3dec9f07b345114230d5867ea7302f8
Dre <aciobirca97@gmail.com>
2023-12-09 15:49:57 +0000

    Upated Projects Section with button, and added SAP Project

--- JS/Home.js
+++ JS/Home.js
@@ -3,7 +3,7 @@ particlesJS.load('hero', 'JS/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-  // particlesJS.load('about', 'JS/particles.json', function() {
-  //   console.log('callback - particles.js config loaded');
-  // });
+  particlesJS.load('particles-js', 'JS/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 
